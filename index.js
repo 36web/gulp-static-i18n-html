55af7d2954bb5eadd40fe796730385a608166c9d
Chris <36web.rocks@gmail.com>
2020-03-29 22:05:18 +0800

    style: formatting and missing semicolons

--- index.js
+++ index.js
@@ -12,57 +12,57 @@ const PLUGIN_NAME = 'gulp-static-i18n-html';
  * from 'static-i18n'
  */
 const DEFAULT_OPTIONS = {
-    locale: 'en',
-    locales: ['en']
-}
+  locale: 'en',
+  locales: ['en']
+};
 
 function gulpStaticI18nHtml(options) {
 
-    // need `this` for file pushing, DO NOT use arrow function.
-    let transform = async function (file, encoding, cb) {
+  // need `this` for file pushing, DO NOT use arrow function.
+  let transform = async function(file, encoding, cb) {
 
-        if (file.isNull()) {
-            return cb(null, file);
-        }
+    if (file.isNull()) {
+      return cb(null, file);
+    }
 
-        if (file.isStream()) {
-            return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
-        }
+    if (file.isStream()) {
+      return cb(new PluginError(PLUGIN_NAME, 'Streaming not supported'));
+    }
 
-        let self = this;
-        options = Object.assign(DEFAULT_OPTIONS, options);
+    let self = this;
+    options = Object.assign(DEFAULT_OPTIONS, options);
 
-        /**
-         * Set 'baseDir' to base of file.
-         * This is typically where a glob starts.
-         */
-        options.baseDir = file.base;
+    /**
+     * Set 'baseDir' to base of file.
+     * This is typically where a glob starts.
+     */
+    options.baseDir = file.base;
 
-        /**
-         * Needs to be 'null' otherwise translated
-         * files are generated automatically
-         */
-        options.outputDir = null;
+    /**
+     * Needs to be 'null' otherwise translated
+     * files are generated automatically
+     */
+    options.outputDir = null;
 
-        const results = await staticI18n.processFile(file.path, options).catch((err) => cb(new PluginError(PLUGIN_NAME, err)));
+    const results = await staticI18n.processFile(file.path, options).catch((err) => cb(new PluginError(PLUGIN_NAME, err)));
 
-        Object.keys(results).forEach((locale) => {
-            let translatedFile = file.clone();
+    Object.keys(results).forEach((locale) => {
+      let translatedFile = file.clone();
 
-            if (options.locale && options.locale === locale) {
-                translatedFile.path = file.path;
-            } else {
-                translatedFile.path = path.join(file.base, locale, file.relative);
-            }
+      if (options.locale && options.locale === locale) {
+        translatedFile.path = file.path;
+      } else {
+        translatedFile.path = path.join(file.base, locale, file.relative);
+      }
 
-            translatedFile.contents = new Buffer.from(results[locale]);
+      translatedFile.contents = new Buffer.from(results[locale]);
 
-            self.push(translatedFile);
-        });
-        cb();
-    };
+      self.push(translatedFile);
+    });
+    cb();
+  };
 
-    return through.obj(transform);
+  return through.obj(transform);
 }
 
 module.exports = gulpStaticI18nHtml;
